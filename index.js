71df1cadf6f1fff706c5d6dd937501ee00ccafa2
maxdahl <github@daagur.de>
2020-01-14 19:36:51 +0100

    deleted auth module to avoid depencies

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const szeObject = require("./src/Object");
 const szeGraphQL = require("./src/GraphQL");
-const szeAuth = require("./src/Auth");
 const szeTime = require("./src/Time");
 
 module.exports = {
   szeObject,
   szeGraphQL,
-  szeAuth,
   szeTime
 };
