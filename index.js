34bc48f39d9d98cdbcfac4095b7bc1b9d74c1bcf
maxdahl <github@daagur.de>
2020-01-15 12:40:26 +0100

    added String module with slugify method

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const szeObject = require("./src/Object");
 const szeGraphQL = require("./src/GraphQL");
 const szeTime = require("./src/Time");
+const szeString = require("./src/String");
 
 module.exports = {
   szeObject,
   szeGraphQL,
-  szeTime
+  szeTime,
+  szeString
 };
